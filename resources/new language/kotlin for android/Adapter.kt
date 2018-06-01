ecaf8334c77ba782f0987e6dfd6a28dea019e7f5
Fay Ho <498130877@qq.com>
2018-06-01 16:44:21 +0800

    Add new language demo
    1. Add Kotlin for Android.
    2. Add Swift for iOS.

---
+++ resources/new language/kotlin for android/Adapter.kt
@@ -0,0 +1,52 @@
+/**
+ *
+ * Copyright (c) 2018 faylib.top
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+package top.faylib.oro.adapter
+
+import android.util.Log
+
+import com.facebook.react.bridge.Callback
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+
+class Adapter(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+
+    //region React-Native Methods
+
+    // 此方法为定义一个给 JavaScript 调用此类的唯一标识名
+    override fun getName(): String {
+        // 返回标识名
+        return "Adapter"
+    }
+
+    // 定义一个给 JavaScript 调用的方法，必须加入 @ReactMethod 关键字，表示声明此方法是提供给 JavaScript 的，方法名是 JavaScript 的调用名，也是唯一标识名，所以此处不能有重名方法，否则最后定义的方法会顶替之前全部的重名方法
+    @ReactMethod
+    fun test(string: String, callback: Callback) {
+        Log.i("OrO", string)
+        callback.invoke("I'm the callback from Kotlin!")
+    }
+
+    //endregion
+}
---
+++ resources/new language/kotlin for android/MainApplication.kt
@@ -0,0 +1,62 @@
+/**
+ *
+ * Copyright (c) 2018 faylib.top
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+package top.faylib.oro.base
+
+import android.app.Application
+
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.shell.MainReactPackage
+import com.facebook.soloader.SoLoader
+
+import top.faylib.oro.BuildConfig
+
+class MainApplication : Application(), ReactApplication {
+
+    private val mReactNativeHost = object : ReactNativeHost(this) {
+        override fun getUseDeveloperSupport(): Boolean {
+            return BuildConfig.DEBUG
+        }
+
+        override fun getPackages(): MutableList<ReactPackage> {
+            // 此处添加一个通信的实例 ReactPackageImpl()
+            return mutableListOf(MainReactPackage(), ReactPackageImpl())
+        }
+
+        override fun getJSMainModuleName(): String {
+            return "index"
+        }
+    }
+
+    override fun getReactNativeHost(): ReactNativeHost {
+        return mReactNativeHost
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, /* native exopackage */ false)
+    }
+}
